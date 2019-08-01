61bb8ba905a93aad75ccb1f0afa93121385602ff
Joshua Klein <mobiusklein@gmail.com>
2019-07-31 21:44:42 -0400

    update docs

--- docs/_build/html/index-1.py
+++ docs/_build/html/index-1.py
@@ -1,5 +1,6 @@
 from brainpy import isotopic_variants
 
+# Generate theoretical isotopic pattern
 peptide = {'H': 53, 'C': 34, 'O': 15, 'N': 7}
 theoretical_isotopic_cluster = isotopic_variants(peptide, npeaks=5, charge=1)
 for peak in theoretical_isotopic_cluster:
@@ -7,19 +8,20 @@ for peak in theoretical_isotopic_cluster:
 
 # produce a theoretical profile using a gaussian peak shape
 import numpy as np
-grid = np.arange(theoretical_isotopic_cluster[0].mz - 1,
-                 theoretical_isotopic_cluster[-1].mz + 1, 0.02)
-intensity = np.zeros_like(grid)
+mz_grid = np.arange(theoretical_isotopic_cluster[0].mz - 1,
+                    theoretical_isotopic_cluster[-1].mz + 1, 0.02)
+intensity = np.zeros_like(mz_grid)
 sigma = 0.002
-for i, mz in enumerate(grid):
-    for peak in theoretical_isotopic_cluster:
-        intensity[i] += peak.intensity * np.exp(-(mz - peak.mz) ** 2 / (2 * sigma)
-                ) / (np.sqrt(2 * np.pi) * sigma)
+for peak in theoretical_isotopic_cluster:
+    # Add gaussian peak shape centered around each theoretical peak
+    intensity += peak.intensity * np.exp(-(mz_grid - peak.mz) ** 2 / (2 * sigma)
+            ) / (np.sqrt(2 * np.pi) * sigma)
 
+# Normalize profile to 0-100
 intensity = (intensity / intensity.max()) * 100
 
 # draw the profile
 from matplotlib import pyplot as plt
-plt.plot(grid, intensity)
+plt.plot(mz_grid, intensity)
 plt.xlabel("m/z")
 plt.ylabel("Relative intensity")
