f0c9cdd59bfc0f225a014e6c12f43726a96dbdc8
Joshua Klein <mobiusklein@gmail.com>
2021-11-06 15:10:17 -0400

    version bump

--- setup.py
+++ setup.py
@@ -119,7 +119,7 @@ def status_msgs(*msgs):
 def run_setup(include_cext=True):
     setup(
         name='brain-isotopic-distribution',
-        version='1.5.9',
+        version='1.5.10',
         packages=find_packages(),
         description="Fast and efficient theoretical isotopic profile generation",
         long_description='''
